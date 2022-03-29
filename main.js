2fb9a9e65354ef187e687e85e161d07f64ec83eb
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-29 22:56:03 +0300

    Add multiple test case for stringLength function

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const stringLength = string => {
+  if (string.length < 1 || string.length > 10) {
+    return null;
+  }
   return string.length;
 }
 
--- main.test.js
+++ main.test.js
@@ -3,3 +3,9 @@ const stringLength = require('./main');
 test("get string length", () => {
   expect(stringLength('abcdef')).toBe(6);
 });
+test("check zero length", () => {
+  expect(stringLength('')).toBe(null);
+});
+test("over max length", () => {
+  expect(stringLength('abcwerwerdef')).toBe(null);
+});
